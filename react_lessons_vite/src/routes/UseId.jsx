5091a88095cc39527aeb99391bcd53ac6170772e
TJCole03 <tjc6254@gmail.com>
2024-01-09 05:58:22 -0500

    UseId notes

--- react_lessons_vite/src/routes/UseId.jsx
+++ react_lessons_vite/src/routes/UseId.jsx
@@ -4,7 +4,10 @@ function LabelInputPair() {
     const id = useId() //initializing hook 
     return (
         <div style={{ marginBottom: '50px' }}>
-            <label htmlFor={id}>
+            <label htmlFor={id}> 
+                {/* matching the htmlFor to the id of input (4 lines down) */}
+                {/* if we have four different inputs, we can't 
+            say id=1 */}
                 Click this label to hilight input {id}
             </label>
             <br />
@@ -12,6 +15,12 @@ function LabelInputPair() {
         </div>
     )
 }
+
+// using LabelInputPair 4 times
+// need this to work on SSR; needs to be consistent across renders
+// useId gives back unique identifier that is consistent with this a function 
+// and can be used multiple times across various different renders
+// when you need and id that PER THE INSTANTIATION of the componnet is consistent
 
 export default function UseIdComponent() {
     return (
