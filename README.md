809670b9ee883a5679967f5d7316587bd73ebd9d
TJCole03 <tjc6254@gmail.com>
2024-01-09 05:48:56 -0500

    UseLayoutEffect notes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # VITE_intermediate_react
+
--- react_lessons_vite/src/routes/UseLayoutEffect.jsx
+++ react_lessons_vite/src/routes/UseLayoutEffect.jsx
@@ -1,14 +1,23 @@
 import { useState, useLayoutEffect, useRef } from 'react'
 
 const LayoutEffectComponent = () => {
+    //your hooks
     const [width, setWidth] = useState(0)
     const [height, setHeight] = useState(0)
     const el = useRef();
+
+    //WHY DON'T WE USE useEffect???
+        //an effect happens immediately after a render ends. an effect happens immediately after a render ends
+        // an effect happens immeiately after a render ends
+        //there is a "pause" in synchronicity (meaning milliseconds) between the render and the effect.
 
     useLayoutEffect(() => {
         setWidth(el.current.clientWidth)
         setHeight(el.current.clientHeight)
     })
+
+    //this hook is useful for if you need to measure something that happens in the DOM
+    //but it can only be measured after its been rendered
 
     return (
     <>
